80b588a84f2e31bc021bb7766dbbeabe9a041e29
gsg0222 <gsg0222.1977@gmail.com>
2023-05-20 11:53:26 +0900

    Spring Boot 3.1.0に対応
    includeが非推奨だったのでinsertに
    対象の指定方法を~{}でラップするように

--- src/main/java/blog/tsuchiya/tutorial/step3/controller/TestController.java
+++ src/main/java/blog/tsuchiya/tutorial/step3/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-	@GetMapping
+	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("title", "コンテンツページ");
 		return "contents";
